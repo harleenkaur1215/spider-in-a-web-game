b6c2d6c2b2f24e172066f1692147bafe9e7d4a5c
Sergio Estevao <smestevao11@gmail.com>
2021-12-15 15:50:27 +0000

    fixed signup and login forms concurrent appearance bug. Signup and login now operational

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 
-function openForm() {
-    document.getElementById("myForm").style.display = "flex";
+function openLoginForm() {
+    //document.getElementById("page-mask").style.display = "flex";
+    document.getElementById("loginForm").style.display = "flex";
+    document.getElementById("signup").disabled = true;
   }
   
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+function closeLoginForm() {
+  //document.getElementById("page-mask").style.display = "none";
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("signup").disabled = false;
 }
+
+function openSignupForm() {
+  //document.getElementById("page-mask").style.display = "flex";
+  document.getElementById("signupForm").style.display = "flex";
+  document.getElementById("login").disabled = true;
+}
+
+function closeSignupForm() {
+  //document.getElementById("page-mask").style.display = "none";
+  document.getElementById("signupForm").style.display = "none";
+  document.getElementById("login").disabled = false;
+}
+
 
 function openHighscores(){
 
