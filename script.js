5f407ce4e67fb228774a849a068e760d56857ac0
Sergio Estevao <smestevao11@gmail.com>
2021-12-15 12:08:54 +0000

    login button added, needs further testing

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openForm() {
+    mancala.getElementById("myForm").style.display = "block";
+  }
+  
+function closeForm() {
+    mancala.getElementById("myForm").style.display = "none";
+}
