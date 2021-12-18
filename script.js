b8649c60562a769651ef74abb0ccd218b4ec69c8
Sergio Estevao <smestevao11@gmail.com>
2021-12-18 15:34:10 +0000

    toggle gamemode button added, difficulty selection need further fixing

--- script.js
+++ script.js
@@ -43,6 +43,28 @@ function closeSignupForm() {
   }
 }
 
+var buttonStage = 0;
+
+function gamemodeButtonClick(){
+  changeGamemode;
+  var background = document.getElementById("toggle_gamemode").style.background;
+  if (buttonStage == 1){
+    document.getElementById("difficulty-input").style.display = "flex";
+    document.getElementById("toggle_gamemode").style.background = "url(images/toggle_left.png)";
+    buttonStage = 0;
+  }
+  else{
+    document.getElementById("difficulty-input").style.display = "none";
+    document.getElementById("toggle_gamemode").style.background = "url(images/toggle_right.png)";
+    buttonStage = 1;
+  }
+}
+
+
+function changeGamemode(){
+
+}
+
 
 function openHighscores(){
 
