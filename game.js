22b912edb21ec79e9d6868e567a83aca9993feff
Sergio Estevao <smestevao11@gmail.com>
2022-01-04 12:30:04 +0000

    changed game.js

---
+++ game.js
@@ -0,0 +1,33 @@
+
+class Seed{
+    constructor(){
+        
+    }
+
+}
+
+class Player{
+    constructor(isHuman){
+        this.isHuman = isHuman;
+    }
+
+}
+
+class Board{
+    constructor(numPits, numSeeds){
+
+    }
+
+}
+
+
+class Game{
+    constructor(board, numPlayers, playerone, playertwo){
+        this.board = board;
+        this.numPlayers = numPlayers;
+        this.playerone = playerone;
+        this.playertwo = playertwo;
+    }
+
+
+}
