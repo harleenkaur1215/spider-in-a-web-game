e1c5bacda6e3a102a385b6e5445c5dad6dedf770
unknown <ricky.ferreira.305@gmail.com>
2022-01-20 01:23:11 +0000

    Board

--- script.js
+++ script.js
@@ -65,10 +65,25 @@ function changeGamemode(){
 
 }
 
+var seedsPerpit = 3;
+var pitsPerPlayer = 6;
+
 function chooseSeedsPerPit(id){
   for(var x of document.getElementsByClassName("seed-buttons")){
     if (x.id == id){
       x.style.background = "Gray";
+      if (x.id == 'threeseeds-button'){
+        seedsPerpit = 3;
+      }
+      if (x.id == 'fourseeds-button'){
+        seedsPerpit = 4;
+      }
+      if (x.id == 'fiveseeds-button'){
+        seedsPerpit = 5;
+      }
+      if (x.id == 'sixseeds-button'){
+        seedsPerpit = 6;
+      }
     }
     else{
       x.style.background = "White";
@@ -80,6 +95,18 @@ function choosePitsPerPlayer(id){
   for(var x of document.getElementsByClassName("pits-buttons")){
     if (x.id == id){
       x.style.background = "Gray";
+      if (x.id == 'fourpits-button'){
+        pitsPerPlayer = 4;
+      }
+      if (x.id == 'fivepits-button'){
+        pitsPerPlayer = 5;
+      }
+      if (x.id == 'sixpits-button'){
+        pitsPerPlayer = 6;
+      }
+      if (x.id == 'sevenpits-button'){
+        pitsPerPlayer = 7;
+      }
     }
     else{
       x.style.background = "White";
@@ -90,6 +117,64 @@ function choosePitsPerPlayer(id){
 
 function startGame(){
   document.getElementById("game-configs").style.display = "none";
+  buildBoard();
+  setGridColumns();
+  document.getElementById("game-board").style.display = "grid";
+}
+
+function setGridColumns(){
+  board.style.setProperty('grid-template-columns', `repeat(${pitsPerPlayer}, 1fr)`)
+}
+
+function buildBoard(){
+  document.getElementById("storage1").value=0;
+  document.getElementById("storage2").value=pitsPerPlayer+1;
+  document.getElementById("storageValue1").value=0;
+  document.getElementById("storageValue2").value=pitsPerPlayer+1;
+  for (let i=pitsPerPlayer; i > 0;i--){
+    var newPit = document .createElement("pit");
+    newPit.classList.add("allPits");
+    newPit.value = i;
+    var divAtual = document.getElementById("board");
+    // for (let j = 0; j < seedsPerpit; j++){
+    //   var newSeed = document.createElement("seed");
+    //   newPit.appendChild(newSeed);
+    // }
+    var newSeed = document.createElement("seed");
+    newPit.appendChild(newSeed);
+    divAtual.appendChild(newPit); 
+  }
+  for (let i=pitsPerPlayer + 2; i < 2*pitsPerPlayer+2;i++){
+    var newPit = document .createElement("pit");
+    newPit.classList.add("allPits");
+    newPit.value = i;
+    var divAtual = document.getElementById("board");
+    // for (let j = 0; j < seedsPerpit; j++){
+    //   var newSeed = document.createElement("seed");
+    //   newPit.appendChild(newSeed);
+    // }
+    var newSeed = document.createElement("seed");
+    newPit.appendChild(newSeed);
+    divAtual.appendChild(newPit); 
+  }
+  for (let i=pitsPerPlayer+2; i < 2*pitsPerPlayer+2;i++){
+    var newSeedValue = document.createElement("seed-value");
+    newSeedValue.classList.add("allValues");
+    newSeedValue.value=i;
+    value = document.createTextNode(seedsPerpit);
+    newSeedValue.appendChild(value);
+    var divAtual = document.getElementById("values1");
+    divAtual.appendChild(newSeedValue);
+  }
+  for (let i=pitsPerPlayer; i > 0;i--){
+    var newSeedValue = document.createElement("seed-value");
+    newSeedValue.classList.add("allValues");
+    newSeedValue.value=i;
+    value = document.createTextNode(seedsPerpit);
+    newSeedValue.appendChild(value);
+    var divAtual = document.getElementById("values2");
+    divAtual.appendChild(newSeedValue);
+  }
 }
 
 
