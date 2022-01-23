7cd3ac20d36cd9d34891babb89b51ae366273460
Sergio Estevao <smestevao11@gmail.com>
2022-01-23 22:20:47 +0000

    bot fixed

--- js/requests.js
+++ js/requests.js
@@ -91,7 +91,7 @@ class Board{
           return -2; //wrongmove
       }
 
-      var seeds = this.pits[cavity].length;
+      let seeds = this.pits[cavity].length;
 
       if(seeds == 0){
           return -2; //wrongmove
@@ -99,9 +99,9 @@ class Board{
 
       this.pits[cavity].length = 0;
 
-      var seedingCav = cavity;
+      let seedingCav = cavity;
 
-      var counter = 0;
+      let counter = 0;
       while(counter < seeds){
           
           seedingCav++;
@@ -214,19 +214,17 @@ class Board{
   }
 
   clone(){
-      var ret = new Board(this.boardSize, 1);
+      let ret = new Board(this.boardSize, 1);
 
-      var pitsCopy = [];
+      let pitsCopy = [];
       for(var c = 0; c < this.boardSize; c++){
           pitsCopy[c] = [];
       }
 
-      for(var n = 1; n < this.boardSize; n++){
-          if(n != (this.boardSize/2)){
+      for(var n = 0; n < this.boardSize; n++){
               for(var x = 0; x < this.pits[n].length; x++){
                   pitsCopy[n].push(new Seed());
               }
-          }
       }
 
       ret.setPits(pitsCopy);
@@ -309,8 +307,8 @@ class Game{
   }
 
   botRandom(){
-      var randomMove;
-      var result;
+      let randomMove;
+      let result;
       do{
           randomMove = Math.floor(Math.random() * (this.boardSize/2 - 1)) + 1;
           result = this.board.move(this.turn, randomMove);
@@ -319,39 +317,64 @@ class Game{
   }
 
   botEasy(){
-      var boardCpy = this.board.clone();
-      var bestMove = bot(boardCpy, 1);
-      this.board.move(this.turn, bestMove);
-      console.log("Insert value: " + bestMove);
+    let ret;
+    let boardCpy;
+    let bestMove;
+    do{
+      boardCpy = this.board.clone();
+      bestMove = bot(boardCpy, 2);
+      ret = this.board.move(2, bestMove);
+
+      if(this.board.checkEndGame()){
+          return this.board.getWinner();
+      }
+    }while(ret == -1)
   }
 
 
   botMedium(){
-      var boardCpy = this.board.clone();
-      var bestMove = bot(boardCpy, 3);
-      this.board.move(this.turn, bestMove);
-      console.log("Insert value: " + bestMove);
+      let ret;
+      let boardCpy;
+      let bestMove;
+      do{
+        boardCpy = this.board.clone();
+        bestMove = bot(boardCpy, 4);
+        ret = this.board.move(2, bestMove);
+
+        if(this.board.checkEndGame()){
+            return this.board.getWinner();
+        }
+      }while(ret == -1)
   }
 
   botHard(){
-      var boardCpy = this.board.clone();
-      var bestMove = bot(boardCpy, 5);
-      this.board.move(this.turn, bestMove);
-      console.log("Insert value: " + bestMove);
+      let ret;
+      let boardCpy;
+      let bestMove;
+      do{
+        boardCpy = this.board.clone();
+        bestMove = bot(boardCpy, 6);
+        ret = this.board.move(2, bestMove);
+
+        if(this.board.checkEndGame()){
+            return this.board.getWinner();
+        }
+      }while(ret == -1)
+    
   }
 
 }
 
 function bot(gameBoard, depth){
-  var maxPointsDif = 0 - (Number.MAX_VALUE);
-  var bestMove;
+  let maxPointsDif = 0 - (Number.MAX_VALUE);
+  let bestMove;
 
 
-  var res;
-  var pointDif;
-  var boardCopy;
-  var moveResult;
-  for(var c = 1; c < gameBoard.getSize()/2; c++){
+  let res;
+  let pointDif;
+  let boardCopy;
+  let moveResult;
+  for(let c = 1; c < gameBoard.getSize()/2; c++){
       boardCopy = gameBoard.clone();
       moveResult = boardCopy.move(2, c);
 
@@ -386,17 +409,17 @@ function minimax(gameBoard, depth, isMax){
       return gameBoard;
   }
 
-  var maxPointsDif = 0 - (Number.MAX_VALUE);
-  var bestBoard;
-  var pointDif;
+  let maxPointsDif = 0 - (Number.MAX_VALUE);
+  let bestBoard = gameBoard;;
+  let pointDif;
 
-  var res;
-  var boardCopy;
-  var moveResult;
-  var pointDif;
+  let res;
+  let boardCopy;
+  let moveResult;
+
 
   if(isMax){
-      for(var c = 1; c < gameBoard.getSize()/2; c++){
+      for(let c = 1; c < gameBoard.getSize()/2; c++){
           boardCopy = gameBoard.clone();
           moveResult = boardCopy.move(2, c);
 
@@ -420,7 +443,7 @@ function minimax(gameBoard, depth, isMax){
       }
   }
   else{
-      for(var c = gameBoard.getSize()/2+1; c < gameBoard.getSize(); c++){
+      for(let c = gameBoard.getSize()/2+1; c < gameBoard.getSize(); c++){
           boardCopy = gameBoard.clone();
           moveResult = boardCopy.move(1, c);
 
@@ -637,6 +660,12 @@ function updateBoard(){
 
 
 function makeMove(pit){
+    if (buttonStage == 1){
+
+    }
+    else{
+
+    }
     let ret = game.move(pit);
     updateBoard();
     console.log(ret);
@@ -667,8 +696,11 @@ function makeMove(pit){
 
 function startGame(){
   document.getElementById("game-configs").style.display = "none";
+  buildBoard();
+  setGridColumns();
+  document.getElementById("game-board").style.display = "grid";
   if(buttonStage == 1){
-      gameMode = 1;
+    join(LoginNick, LoginPassword, pitsPerPlayer, seedsPerpit);
   }
   else{
       let difficulty = document.getElementById("difficulty");
@@ -680,9 +712,7 @@ function startGame(){
 //   else{
 //       start = 1;
 //   }
-  buildBoard();
-  setGridColumns();
-  document.getElementById("game-board").style.display = "grid";
+  
   game = new Game(2*pitsPerPlayer+2, seedsPerpit, 1, gameMode);
   updateBoard();
 
@@ -752,6 +782,29 @@ function closeCommands(){
 }
 
 
+function hideForms(){
+    for (var x of document.getElementsByClassName("registerForms")){
+        x.style.display= "none";
+    }
+}
+
+function giveRegisterError(){
+    document.getElementById("feedbackLogin").innerHTML = "Credentials Wrong";
+    
+}
+
+function hideForms(){
+    document.getElementById("feedbackLogin").innerHTML = " ";
+    closeLoginForm();
+}
+
+function login(){
+  var nick = document.getElementById("loginNick").value;
+  var pass = document.getElementById("loginPass").value;
+  register(nick, pass)
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -798,8 +851,20 @@ const sendHttpRequest = (method, url, data) => {
 /*http://twserver.alunos.dcc.fc.up.pt:8008/*/
 
 var LoginNick;
-
 var LoginPassword;
+var game_id;
+var error_msg;
+var player_ranking;
+var server_response;
+
+function addTestEvent(){
+    document.getElementById('Butao').addEventListener("click", function(){ranking();});
+    document.getElementById('leavebt').addEventListener("click", function(){leave(game_id, LoginNick, LoginPassword);});
+
+}
+
+addTestEvent();
+
 
 /*Register*/ 
 
@@ -820,46 +885,98 @@ const register = (nickname, pass) => {
       });
 };
 
-function hideForms(){
-    for (var x of document.getElementsByClassName("registerForms")){
-        x.style.display= "none";
-    }
-}
-
-function giveRegisterError(){
-    document.getElementById("feedbackLogin").innerHTML = "Credentials Wrong";
-    
-}
-
-function hideForms(){
-    document.getElementById("feedbackLogin").innerHTML = " ";
-    closeLoginForm();
-}
-
-function login(){
-  var nick = document.getElementById("loginNick").value;
-  var pass = document.getElementById("loginPass").value;
-  register(nick, pass)
-}
-
 /*join*/
 
-const join = (nickname, pass, siz, init) => {
+const join = (nickname, pass, pitsPerPlayer, seedsPerpit) => {
   sendHttpRequest('POST', 'http://twserver.alunos.dcc.fc.up.pt:8008/join', {
     group: 71101,
     nick: nickname,
     password: pass,
-    size: siz,
-    initial: init
-
+    size: pitsPerPlayer,
+    initial: seedsPerpit
+    
   })
     .then(responseData => {
       console.log(responseData);
-      
+      game_id = responseData.game;
+      update();
     })
     .catch(err => {
       console.log(err, err.data);
-      
+      document.innerHtml = "You need to be logged in first"
     });
 };
 
+
+
+/*leave*/
+
+const leave = (game_id, nickname, pass) => {
+    sendHttpRequest('POST', 'http://twserver.alunos.dcc.fc.up.pt:8008/leave', {
+      game: game_id,
+      nick: nickname,
+      password: pass
+      
+    })
+      .then(responseData => {
+        console.log(responseData);
+        
+      })
+      .catch(err => {
+        console.log(err, err.data);
+        
+      });
+  };
+
+
+/*notify*/
+
+const notify = (nickname, pass, game_id, move_input) => {
+    sendHttpRequest('POST', 'http://twserver.alunos.dcc.fc.up.pt:8008/notify', {
+        nick: nickname,
+        password: pass,
+        game: game_id,
+        move: move_input
+        
+      })
+      .then(responseData => {
+        console.log(responseData);
+        
+      })
+      .catch(err => {
+        console.log(err, err.data);
+        error = err.data;
+      });
+  };
+
+
+
+/*ranking*/
+
+const ranking = () => {
+    sendHttpRequest('POST', 'http://twserver.alunos.dcc.fc.up.pt:8008/ranking', {})
+      .then(responseData => {
+        console.log(responseData);
+        player_ranking = responseData;
+      })
+      .catch(err => {
+        console.log(err, err.data);
+        error = err.data;
+      });
+  };
+
+
+/*update*/
+
+  const update = () => {
+    let game_server = new EventSource('http://twserver.alunos.dcc.fc.up.pt:8008/update?nick=' + LoginNick +'&game='+ game_id);
+    game_server.onmessage = response => {
+        let server_a = JSON.parse(response.data)
+        console.log(server_a);
+    
+    }
+    game_server.onerror = error => {
+        error_msg = JSON.parse(error)
+        console.log(error_msg);
+    }
+}
